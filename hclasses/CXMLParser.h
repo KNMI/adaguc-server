de0fdb28c5c249f86c92c822971f753bb8fa8472
Maarten Plieger <plieger@knmi.nl>
2013-06-03 17:09:51 +0200

    Added Apache 2 License

--- hclasses/CXMLParser.h
+++ hclasses/CXMLParser.h
@@ -1,24 +1,28 @@
-/* 
- * Copyright (C) 2012, Royal Netherlands Meteorological Institute (KNMI)
+/******************************************************************************
+ * 
+ * Project:  Helper classes
+ * Purpose:  Generic functions
+ * Author:   Maarten Plieger, plieger "at" knmi.nl
+ * Date:     2013-06-01
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or any 
- * later version.
+ ******************************************************************************
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Copyright 2013, Royal Netherlands Meteorological Institute (KNMI)
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Project    : ADAGUC
- *
- * initial programmer :  M.Plieger
- * initial date       :  20120610
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ ******************************************************************************/
+
 
 #ifndef CXMLPARSER_H
 #define CXMLPARSER_H
