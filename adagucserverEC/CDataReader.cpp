3df3ffba772b935d900019a4770128676cb6abd0
Ernst de Vreede <vreedede@knmi.nl>
2015-02-02 13:31:20 +0000

    Fixed misplaced points array clear()

--- adagucserverEC/CDataReader.cpp
+++ adagucserverEC/CDataReader.cpp
@@ -728,6 +728,16 @@ int CDataReader::open(CDataSource *dataSource,int mode,int x,int y){
   writeLogFile2(dumpString.c_str());*/
   
   
+  for(size_t varNr=0;varNr<dataSource->getNumDataObjects();varNr++){
+    //Check if our variable has a statusflag
+    std::vector<CDataSource::StatusFlag*> *statusFlagList=&dataSource->getDataObject(varNr)->statusFlagList;
+    CDataSource::readStatusFlags(dataSource->getDataObject(varNr)->cdfVariable,statusFlagList);
+    if(statusFlagList->size()>0){
+      dataSource->getDataObject(varNr)->hasStatusFlag=true;
+    }
+    dataSource->getDataObject(varNr)->points.clear();
+  }
+
   
   if(parseDimensions(dataSource,mode,x,y)!=0){
     CDBError("Unable to parseDimensions");
@@ -866,16 +876,6 @@ int CDataReader::open(CDataSource *dataSource,int mode,int x,int y){
   CDPPExecutor DPPExecutor;
   DPPExecutor.executeProcessors(dataSource,CDATAPOSTPROCESSOR_RUNBEFOREREADING);
   
-
-  for(size_t varNr=0;varNr<dataSource->getNumDataObjects();varNr++){
-    //Check if our variable has a statusflag
-    std::vector<CDataSource::StatusFlag*> *statusFlagList=&dataSource->getDataObject(varNr)->statusFlagList;
-    CDataSource::readStatusFlags(dataSource->getDataObject(varNr)->cdfVariable,statusFlagList);
-    if(statusFlagList->size()>0){
-      dataSource->getDataObject(varNr)->hasStatusFlag=true;
-    }
-    dataSource->getDataObject(varNr)->points.clear();
-  }
 
 
   if(mode==CNETCDFREADER_MODE_GET_METADATA){
