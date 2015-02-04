940267bd442e2297f5ca4716dbe2c02437d5a905
Maarten Plieger <plieger@knmi.nl>
2015-02-04 16:40:40 +0100

    Changed version.

--- adagucserverEC/Definitions.h
+++ adagucserverEC/Definitions.h
@@ -26,7 +26,7 @@
 #ifndef Definitions_H
 #define Definitions_H
 
-#define ADAGUCSERVER_VERSION             "1.6"
+#define ADAGUCSERVER_VERSION             "1.7"
 
 //CConfigReaderLayerType
 #define CConfigReaderLayerTypeUnknown     0
