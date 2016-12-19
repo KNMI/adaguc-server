d5a2a850812e963ec2c43ed86a59b475c876d2fa
Maarten Plieger <plieger@knmi.nl>
2016-12-19 14:47:43 +0100

    Added support for STRING attributes

--- CCDFDataModel/CCDFDataModel.cpp
+++ CCDFDataModel/CCDFDataModel.cpp
@@ -29,17 +29,22 @@ void CDF::_dumpPrintAttributes(const char *variableName, std::vector<CDF::Attrib
   //print attributes:
   for(size_t a=0;a<attributes.size();a++){
     CDF::Attribute *attr=attributes[a];
-    dumpString->printconcat("\t\t%s:%s =",variableName,attr->name.c_str());
+    if(attr->type!=CDF_STRING){
+      dumpString->printconcat("\t\t%s:%s =",variableName,attr->name.c_str());
+    }else{
+      dumpString->printconcat("\t\tstring %s:%s =",variableName,attr->name.c_str());
+    }
     
     //print data
-    char *data = new char[attr->length+1];
-    memcpy(data,attr->data,attr->length);
-    data[attr->length]='\0';
-    if(attr->type==CDF_CHAR)dumpString->printconcat(" \"%s\"",data);
-    //if(attr->type==CDF_UBYTE)dumpString->printconcat(" \"%s\"",data);
-    //if(attr->type==CDF_BYTE)dumpString->printconcat(" \"%s\"",data);
-    delete[] data;
-    
+    if(attr->type==CDF_CHAR){
+      char *data = new char[attr->length+1];
+      memcpy(data,attr->data,attr->length);
+      data[attr->length]='\0';
+      dumpString->printconcat(" \"%s\"",data);
+      //if(attr->type==CDF_UBYTE)dumpString->printconcat(" \"%s\"",data);
+      //if(attr->type==CDF_BYTE)dumpString->printconcat(" \"%s\"",data);
+      delete[] data;
+    }
     
     if(attr->type==CDF_BYTE)for(size_t n=0;n<attr->length;n++)dumpString->printconcat(" %db",((char*)attr->data)[n]);
     if(attr->type==CDF_UBYTE)for(size_t n=0;n<attr->length;n++)dumpString->printconcat(" %dub",((unsigned char*)attr->data)[n]);
@@ -49,6 +54,7 @@ void CDF::_dumpPrintAttributes(const char *variableName, std::vector<CDF::Attrib
     if(attr->type==CDF_USHORT)for(size_t n=0;n<attr->length;n++)dumpString->printconcat(" %d",((unsigned short*)attr->data)[n]);
     if(attr->type==CDF_FLOAT)for(size_t n=0;n<attr->length;n++)dumpString->printconcat(" %ff",((float*)attr->data)[n]);
     if(attr->type==CDF_DOUBLE)for(size_t n=0;n<attr->length;n++)dumpString->printconcat(" %fdf",((double*)attr->data)[n]);
+    if(attr->type==CDF_STRING)for(size_t n=0;n<attr->length;n++)dumpString->printconcat(" \"%s\"",((char**)attr->data)[n]);
     dumpString->printconcat(" ;\n");
   }
 }
