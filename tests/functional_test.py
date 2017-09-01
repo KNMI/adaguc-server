10161e2cb7dc9bd11c16aa9ffaa7fc99ec06cde1
maartenplieger <maartenplieger@gmail.com>
2017-09-01 14:36:12 +0200

    First two functional tests for adaguc-server with PyTest

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,61 +1,79 @@
-import subprocess
 import os
-from os.path import expanduser
-from PIL import Image
 from StringIO import StringIO
 from adaguc.CGIRunner import CGIRunner
-import io
-import tempfile
-import shutil
-
-url="source=testdata.nc&SERVICE=WMS&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=testdata&WIDTH=256&HEIGHT=256&CRS=EPSG%3A4326&BBOX=30,-30,75,30&STYLES=testdata%2Fnearest&FORMAT=image/png&TRANSPARENT=FALSE&"
-
-url="source=testdata.nc&SERVICE=WMS&request=getcapabilities"
-
-adagucenv=os.environ.copy()
-
-ADAGUC_TMP = os.environ['ADAGUC_TMP']
-ADAGUC_PATH = os.environ['ADAGUC_PATH']
-ADAGUC_LOGFILE = os.environ['ADAGUC_LOGFILE']
-adagucexecutable = ADAGUC_PATH+"/bin/adagucserver";
-
-print ("Starting %s" % adagucexecutable)
-
-filetogenerate =  StringIO()
-status, headers = CGIRunner().run([adagucexecutable],url,output = filetogenerate,extraenv=adagucenv)
-
-def printLogFile():
-  print "--------- START ADAGUC LOGS ---------"
-  f=open(ADAGUC_LOGFILE)
-  print f.read()
-  f.close()
-  print "--------- END ADAGUC LOGS ---------"
+import unittest
 
 
-if status != 0:
-  printLogFile()
-  
-  print ("Adaguc-server has non zero exit status %d ", status)
-  if status == -9: print("Process: Killed")
-  if status == -11: print("Process: Segmentation Fault ")
-  
-  if len(headers)!=0: 
-    print("Process: No HTTP Headers written")
-    print headers
 
-  
-else:  
-  try:
-    """ Try to show the image """
-    img = Image.open(StringIO(filetogenerate.getvalue()))
-    img.show()
-  except Exception as e: 
-    print(e)
-    
-    f = (StringIO(filetogenerate.getvalue()))
-    print "Data: [" + f.read() + "]"
-      
-    """ Otherwise print the logfile, giving information on what went wrong """
+def runADAGUCServer(url):
+  def getLogFile():
+    ADAGUC_LOGFILE = os.environ['ADAGUC_LOGFILE']
+    f=open(ADAGUC_LOGFILE)
+    data=f.read()
+    f.close()
+    return data
+
+  def printLogFile():
+    ADAGUC_LOGFILE = os.environ['ADAGUC_LOGFILE']
+    print "--------- START ADAGUC LOGS ---------"
+    print getLogFile()
+    print "--------- END ADAGUC LOGS ---------"
+
+  adagucenv=os.environ.copy()
+
+  ADAGUC_TMP = os.environ['ADAGUC_TMP']
+  ADAGUC_PATH = os.environ['ADAGUC_PATH']
+ 
+  adagucexecutable = ADAGUC_PATH+"/bin/adagucserver";
+
+  print ("Starting %s" % adagucexecutable)
+
+  filetogenerate =  StringIO()
+  status, headers = CGIRunner().run([adagucexecutable],url,output = filetogenerate,extraenv=adagucenv)
+
+
+  if status != 0:
     printLogFile()
-    pass
+    
+    print ("Adaguc-server has non zero exit status %d ", status)
+    if status == -9: print("Process: Killed")
+    if status == -11: print("Process: Segmentation Fault ")
+    
+    if len(headers)!=0: 
+      print("Process: No HTTP Headers written")
+      print headers
+    return [status]
 
+    
+  else:  
+    return [0,filetogenerate]
+    
+def writetofile(filename, data):
+  with open(filename, 'w') as f:
+    f.write(data)
+
+def readfromfile(filename):
+  with open(filename, 'r') as f:
+    return f.read()
+
+class TestStringMethods(unittest.TestCase):
+    overWriteExpectedData = False
+    
+    def test_WMSGetCapabilities_testdatanc(self):
+        filename="expectedoutputs/test_WMSGetCapabilities_testdatanc"
+        status,data = runADAGUCServer("source=testdata.nc&SERVICE=WMS&request=getcapabilities")
+        if self.overWriteExpectedData: writetofile(filename,data.getvalue())
+        self.assertEqual(status, 0)
+        self.assertEqual(data.getvalue(), readfromfile(filename))
+
+    def test_WMSGetMap_testdatanc(self):
+        filename="expectedoutputs/test_WMSGetMap_testdatanc"
+        status,data = runADAGUCServer("source=testdata.nc&SERVICE=WMS&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=testdata&WIDTH=256&HEIGHT=256&CRS=EPSG%3A4326&BBOX=30,-30,75,30&STYLES=testdata%2Fnearest&FORMAT=image/png&TRANSPARENT=FALSE&")
+        if self.overWriteExpectedData: writetofile(filename,data.getvalue())
+        self.assertEqual(status, 0)
+        self.assertEqual(data.getvalue(), readfromfile(filename))
+
+   
+
+if __name__ == '__main__':
+    unittest.main()
