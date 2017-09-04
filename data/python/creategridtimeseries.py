d5ee8aea193c3ef0c583c00633db6d8c30cd0dcf
maartenplieger <maartenplieger@gmail.com>
2017-09-04 18:25:05 +0200

    Added testing 5Dimensional NetCDF files

--- data/python/creategridtimeseries.py
+++ data/python/creategridtimeseries.py
@@ -6,116 +6,145 @@ from PIL import Image
 from PIL import ImageDraw, ImageFont
 
 
-font = ImageFont.truetype("../fonts/OpenSans-Semibold.ttf", 20)
+font = ImageFont.truetype("../fonts/FreeSans.ttf", 12)
 
-fileOutDir="../datasets/netcdf_5dims_seq1.nc"
-dims = {
-  'time':{
-    'vartype':'d',
-    'units':"seconds since 1970-01-01 00:00:00",
-    'standard_name':'time',
-    'values':[netCDF4.date2num(datetime.datetime(2017,01,01,00,00,00), "seconds since 1970-01-01 00:00:00"),
-              netCDF4.date2num(datetime.datetime(2017,01,01,00,05,00), "seconds since 1970-01-01 00:00:00"),
-              netCDF4.date2num(datetime.datetime(2017,01,01,00,10,00), "seconds since 1970-01-01 00:00:00"),
-              netCDF4.date2num(datetime.datetime(2017,01,01,00,15,00), "seconds since 1970-01-01 00:00:00")]
+fileOutDir="../datasets"
+dimsset = {
+  'netcdf_5dims_seq1.nc':{
+    'time':{
+      'vartype':'d',
+      'units':"seconds since 1970-01-01 00:00:00",
+      'standard_name':'time',
+      'values':[netCDF4.date2num(datetime.datetime(2017,01,01,00,00,00), "seconds since 1970-01-01 00:00:00"),
+                netCDF4.date2num(datetime.datetime(2017,01,01,00,05,00), "seconds since 1970-01-01 00:00:00"),
+                netCDF4.date2num(datetime.datetime(2017,01,01,00,10,00), "seconds since 1970-01-01 00:00:00")]
+      },
+    'height':{
+      'vartype':'d',
+      'units':"meters",
+      'standard_name':'height',    
+      'values':[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
+      },
+    'member':{
+      'vartype':str,
+      'units':"member number",
+      'standard_name':'member',
+      'values':['member6','member5','member4','member3','member2','member1']
+      }
     },
-  'height':{
-    'vartype':'d',
-    'units':"meters",
-    'standard_name':'height',    
-    'values':[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
-    },
-  'member':{
-    'vartype':str,
-    'units':"member number",
-    'standard_name':'member',
-    'values':['member6','member5','member4','member3','member2','member1']
-    }
-  }
+'netcdf_5dims_seq2.nc':{
+    'time':{
+      'vartype':'d',
+      'units':"seconds since 1970-01-01 00:00:00",
+      'standard_name':'time',
+      'values':[netCDF4.date2num(datetime.datetime(2017,01,01,00,15,00), "seconds since 1970-01-01 00:00:00"),
+                netCDF4.date2num(datetime.datetime(2017,01,01,00,20,00), "seconds since 1970-01-01 00:00:00"),
+                netCDF4.date2num(datetime.datetime(2017,01,01,00,25,00), "seconds since 1970-01-01 00:00:00")]
+      },
+    'height':{
+      'vartype':'d',
+      'units':"meters",
+      'standard_name':'height',    
+      'values':[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
+      },
+    'member':{
+      'vartype':str,
+      'units':"member number",
+      'standard_name':'member',
+      'values':['member6','member5','member4','member3','member2','member1']
+      }
+    }    
+}
+    
 
-dimstring = []
-for a in dims:
-  dimstring.append(a);
-  print dims[a]['values']
-  
-dimstring.append('lat')  
-dimstring.append('lon')
-print dimstring
+for filename in dimsset:
+  dims = dimsset[filename]
+  dimstring = []
+  for a in dims:
+    dimstring.append(a);
+    print dims[a]['values']
+    
+  dimstring.append('lat')  
+  dimstring.append('lon')
+  print dimstring
 
-latvar = []
-lonvar = []
-latvar = list(reversed(range(-90,90)))
-lonvar = range(-180,180)
+  latvar = []
+  lonvar = []
+  latvar = list(reversed(np.arange(-90. + (2./180.), 90 + (2./180.), 1.0)))
+  lonvar = np.arange(-180. + (2./360.),180 + (2./360.),1.0)
 
-im2 = Image.new("L", (len(lonvar),len(latvar)))
+  print (len(lonvar),len(latvar))
 
-
-draw = ImageDraw.Draw(im2) 
-
-draw.fontmode = "1"
-
-ncfile = netCDF4.Dataset(fileOutDir,'w')
-lon_dim = ncfile.createDimension('lon', len(lonvar))
-lat_dim = ncfile.createDimension('lat', len(latvar))
-
-lat = ncfile.createVariable('lat', 'f4', ('lat'),)
-lat.units = 'degrees_north'
-lat.standard_name = 'latitude'
-lon = ncfile.createVariable('lon', 'f4', ('lon'))
-lon.units = 'degrees_east'
-lon.standard_name = 'longitude'
-
-for dim in dims:
-  d=ncfile.createDimension(dim, len(dims[dim]['values']))
-  v =  ncfile.createVariable(dim, dims[dim]['vartype'], (dim))
-  v.units=dims[dim]['units']
-  v.standard_name=dims[dim]['standard_name']
-  if dims[dim]['vartype'] is str:
-    v[:] = np.array((dims[dim]['values']),dtype='object')
-  else:
-    v[:] = dims[dim]['values']
+  im2 = Image.new("L", (len(lonvar),len(latvar)))
 
 
-dataVar = ncfile.createVariable('data','B',dimstring,zlib=True,least_significant_digit=3)
-dataVar.units = 'km'
-dataVar.standard_name = 'data'
+  draw = ImageDraw.Draw(im2) 
 
+  draw.fontmode = "1"
+  filepath = fileOutDir +"/"+filename
+  print "writing to " + filepath
+  ncfile = netCDF4.Dataset(filepath,'w')
+  lon_dim = ncfile.createDimension('lon', len(lonvar))
+  lat_dim = ncfile.createDimension('lat', len(latvar))
 
-lat[:] = [latvar]
-lon[:] = [lonvar]
+  lat = ncfile.createVariable('lat', 'f4', ('lat'),)
+  lat.units = 'degrees_north'
+  lat.standard_name = 'latitude'
+  lon = ncfile.createVariable('lon', 'f4', ('lon'))
+  lon.units = 'degrees_east'
+  lon.standard_name = 'longitude'
 
-
-def Recurse (dims, number, l):
-   for value in range(len(dims[dims.keys()[number-1]]['values'])):
-    l[number-1] = value
-    if number > 1:
-        Recurse ( dims, number - 1 ,l)
+  for dim in dims:
+    d=ncfile.createDimension(dim, len(dims[dim]['values']))
+    v =  ncfile.createVariable(dim, dims[dim]['vartype'], (dim))
+    v.units=dims[dim]['units']
+    v.standard_name=dims[dim]['standard_name']
+    if dims[dim]['vartype'] is str:
+      v[:] = np.array((dims[dim]['values']),dtype='object')
     else:
-      draw.rectangle(((0,0,len(lonvar),len(latvar))), fill = 0)
-      draw.text((5, 5), str(l), font=font,  fill=255)
-      
-      for i in range(len(l)):
-        value = (dims[dims.keys()[i]]['values'])[l[i]]
-        units = dims[dims.keys()[i]]['units']
-        standardname = dims[dims.keys()[i]]['standard_name']
+      v[:] = dims[dim]['values']
+
+
+  dataVar = ncfile.createVariable('data','B',dimstring,zlib=True,least_significant_digit=3)
+  dataVar.units = 'km'
+  dataVar.standard_name = 'data'
+
+
+  lat[:] = [latvar]
+  lon[:] = [lonvar]
+
+
+  def Recurse (dims, number, l):
+    for value in range(len(dims[dims.keys()[number-1]]['values'])):
+      l[number-1] = value
+      if number > 1:
+          Recurse ( dims, number - 1 ,l)
+      else:
+        draw.rectangle(((0,0,len(lonvar),len(latvar))), fill = 0)
+        draw.text((5, 5), str(l), font=font,  fill=255)
         
-        if standardname is "time":
-          value =  netCDF4.num2date(value, units);
-        draw.text((5, 50 + i*20), str(value), font=font,  fill=255)
+        for i in range(len(l)):
+          value = (dims[dims.keys()[i]]['values'])[l[i]]
+          units = dims[dims.keys()[i]]['units']
+          standardname = dims[dims.keys()[i]]['standard_name']
+          
+          if standardname is "time":
+            value =  netCDF4.num2date(value, units);
+          draw.text((5, 50 + i*20), str(value), font=font,  fill=255)
 
-      draw.text((5, 25), fileOutDir, font=font,  fill=255)
-      
-      
-      datavar=np.reshape(list(im2.getdata(0)), (180,360))
+        draw.text((5, 25), filepath, font=font,  fill=255)
+        
+        
+        datavar=np.reshape(list(im2.getdata(0)), (180,360))
 
-      dataVar[l[0],l[1],l[2],:,:] = datavar
+        dataVar[l[0],l[1],l[2],:,:] = datavar
 
-      
-l = []
+        
+  l = []
 
-for i in range(len(dims)):
-      l.append(0)      
-Recurse(dims,len(dims),l)
+  for i in range(len(dims)):
+        l.append(0)      
+  Recurse(dims,len(dims),l)
 
-ncfile.Conventions = "CF-1.4";
-ncfile.close()
+  ncfile.Conventions = "CF-1.4";
+  ncfile.close()
