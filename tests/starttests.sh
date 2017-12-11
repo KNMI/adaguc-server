fadf21393b2170d55a63f6a56e041d69a1720b60
maartenplieger <maarten.plieger@knmi.nl>
2017-12-11 16:31:44 +0100

    Fixed functional tests

--- tests/starttests.sh
+++ tests/starttests.sh
@@ -15,4 +15,8 @@ export ADAGUC_CONFIG="${ADAGUC_PATH}/data/config/adaguc.autoresource.xml"
 export ADAGUC_FONT="${ADAGUC_PATH}/data/fonts/FreeSans.ttf"
 export ADAGUC_ONLINERESOURCE=""
 
-python ${ADAGUC_PATH}/tests/functional_test.py
+python ${ADAGUC_PATH}/tests/functional_test.py $1
+
+
+# To run a specific test:
+# bash starttests.sh TestStringMethods.test_WMSCMDUpdateDBTailPath
