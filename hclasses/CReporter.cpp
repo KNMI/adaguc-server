432d1a8978da3fdd49c605242bf0f325a77215e9
Ian van der Neut <neutvd@knmi.nl>
2018-04-06 11:21:15 +0200

    Fixed message indicators from CReporter.
    For debugging purposes it was nice to be able to grep on what messages
    in the log file came from the CReporter class. So they were different.
    This commit makes them te same as the indicators from the logging
    functions.

--- hclasses/CReporter.cpp
+++ hclasses/CReporter.cpp
@@ -34,7 +34,7 @@ CReporter *CReporter::getInstance() {
 void CReporter::addError(const CT::string error, const char* file, int line, const char* className) {
     if (this->writelog) {
         std::ostringstream error_str;
-        error_str << "[EEE:" << file << ", " << " " << line << " in " << className << "] "
+        error_str << "[E:" << file << ", " << " " << line << " in " << className << "] "
                 << error.c_str() << std::endl;
         _printErrorStreamPointer(error_str.str().c_str());
     }
@@ -44,7 +44,7 @@ void CReporter::addError(const CT::string error, const char* file, int line, con
 void CReporter::addWarning(const CT::string warning, const char* file, int line, const char* className) {
     if (this->writelog) {
         std::ostringstream warn_str;
-        warn_str << "[WWW:" << file << ", " << " " << line << " in " << className << "] "
+        warn_str << "[W:" << file << ", " << " " << line << " in " << className << "] "
                 << warning.c_str() << std::endl;
         _printWarningStreamPointer(warn_str.str().c_str());
     }
@@ -54,7 +54,7 @@ void CReporter::addWarning(const CT::string warning, const char* file, int line,
 void CReporter::addInfo(const CT::string infoMessage, const char* file, int line, const char* className) {
     if (this->writelog) {
         std::ostringstream dbg_str;
-        dbg_str << "[DDDD:" << file << ", " << " " << line << " in " << className << "] "
+        dbg_str << "[D:" << file << ", " << " " << line << " in " << className << "] "
                 << infoMessage.c_str() << std::endl;
         _printDebugStreamPointer(dbg_str.str().c_str());
     }
