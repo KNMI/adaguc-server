fa38b7c77377b8757d15fac5b52afa4d716ccd08
Maarten Plieger <maartenplieger@gmail.com>
2018-06-25 14:11:31 +0200

    [adaguc-server-updatedatasets.sh] Remove all old service status files such that only active services are monitored

--- Docker/adaguc-server-updatedatasets.sh
+++ Docker/adaguc-server-updatedatasets.sh
@@ -3,13 +3,11 @@ export ADAGUC_PATH=/adaguc/adaguc-server-master/
 export ADAGUC_TMP=/tmp
 
 if [[ $1 ]]; then
-  # remove all old service such that only active services are monitored
-  
-  
   # Update a specific dataset
   for configfile in /data/adaguc-datasets/$1.xml ;do
     filename=/data/adaguc-datasets/"${configfile##*/}" 
     filebasename=${filename##*/}
+    # remove all old service status file such that only active services are monitored
     rm -f /servicehealth/${filebasename%.*}
     echo "Starting update for ${filename}" 
     /adaguc/adaguc-server-master/bin/adagucserver --updatedb --config /adaguc/adaguc-server-config.xml,${filename}
@@ -18,7 +16,7 @@ if [[ $1 ]]; then
   done
 
 else
-  # remove all old services such that only active services are monitored
+  # remove all old service status files such that only active services are monitored
   rm -f /servicehealth/*
   # Update all datasets
   for configfile in /data/adaguc-datasets/*xml ;do
