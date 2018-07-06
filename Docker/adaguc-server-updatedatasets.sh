c4233c39054c07978bf10eb0f2e0dbd65c8b62ef
Maarten Plieger <maartenplieger@gmail.com>
2018-07-06 14:01:26 +0200

    Added extra feedback information on update datasets.sh

--- Docker/adaguc-server-updatedatasets.sh
+++ Docker/adaguc-server-updatedatasets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export ADAGUC_PATH=/adaguc/adaguc-server-master/
 export ADAGUC_TMP=/tmp
+export ADAGUC_ONLINERESOURCE=""
 
 if [[ $1 ]]; then
   # Update a specific dataset
@@ -9,16 +10,18 @@ if [[ $1 ]]; then
     filebasename=${filename##*/}
     # remove all old service status file such that only active services are monitored
     rm -f /servicehealth/${filebasename%.*}
+    echo ""
     if [[ $2 ]]; then
-      echo "Starting update with tailparth $2 for dataset ${filename}" 
+      echo "*** Starting update with tailparth $2 for dataset ${filename}" 
       /adaguc/adaguc-server-master/bin/adagucserver --updatedb --config /adaguc/adaguc-server-config.xml,${filename} --tailpath $2
       OUT=$?
     else
-      echo "Starting update for ${filename}" 
+      echo "*** Starting update for ${filename}" 
       /adaguc/adaguc-server-master/bin/adagucserver --updatedb --config /adaguc/adaguc-server-config.xml,${filename}
       OUT=$?
     fi
     echo "$OUT" > /servicehealth/${filebasename%.*}
+    echo ""
   done
 
 else
@@ -28,6 +31,7 @@ else
   for configfile in /data/adaguc-datasets/*xml ;do
     filename=/data/adaguc-datasets/"${configfile##*/}" 
     filebasename=${filename##*/}
+    echo ""
     echo "Starting update for ${filename}" 
     /adaguc/adaguc-server-master/bin/adagucserver --updatedb --config /adaguc/adaguc-server-config.xml,${filename}
     OUT=$?
