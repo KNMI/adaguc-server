55141bfdf62e9ad56bed2812b1d887115c9f5134
plieger <plieger@bhw485.knmi.nl>
2018-09-28 12:11:41 +0000

    Added ADAGUC_Feature functions

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -5,15 +5,15 @@ from AdagucTests.TestWMSDocumentCache import TestWMSDocumentCache
 from AdagucTests.TestOpenDAPServer import TestOpenDAPServer
 from AdagucTests.TestWMSTiling import TestWMSTiling
 from AdagucTests.TestWMSPolylineRenderer import TestWMSPolylineRenderer
-#from AdagucTests.TestADAGUCFeatureFunctions import TestADAGUCFeatureFunctions
+from AdagucTests.TestADAGUCFeatureFunctions import TestADAGUCFeatureFunctions
 
 testsuites=[];
-testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestWMS))
-testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestWMSDocumentCache))
-testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestOpenDAPServer))
-testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestWMSPolylineRenderer))
+#testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestWMS))
+#testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestWMSDocumentCache))
+#testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestOpenDAPServer))
+#testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestWMSPolylineRenderer))
 #testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestWMSTiling))
-#testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestADAGUCFeatureFunctions))
+testsuites.append(unittest.TestLoader().loadTestsFromTestCase(TestADAGUCFeatureFunctions))
 result=unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(testsuites))
 
 
