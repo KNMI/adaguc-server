e98a407800d5bf6cbbdcaddd1a91b229a0c218dc
simonvanbennekom <simonvanbennekom@gmail.com>
2018-11-16 14:02:13 +0000

    Add first implementation for posting on ADAGUC in CRequest.cpp, added some classes to process this. Cleaned some code in the SLD implementation

---
+++ adagucserverEC/CSLDPostRequest.h
@@ -0,0 +1,26 @@
+//
+// Created by bennekom on 11/16/18.
+//
+
+#ifndef PROJECT_CSLDPOSTREQUEST_H
+#define PROJECT_CSLDPOSTREQUEST_H
+
+#endif //PROJECT_CSLDPOSTREQUEST_H
+
+#include "CDebugger.h"
+#include "CTString.h"
+
+class CSLDPostRequest {
+
+public:
+  CSLDPostRequest();
+
+  static int startProcessing(CT::string * post_body);
+
+private:
+  DEF_ERRORFUNCTION();
+};
+
+
+
+
