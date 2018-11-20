d2b4eea4d69327d669f51f423346b663c33511bf
maartenplieger <maartenplieger@gmail.com>
2018-11-20 09:27:59 +0000

    New adaguc-services 1.0.13

--- Docker/docker-compose-generate-env.sh
+++ Docker/docker-compose-generate-env.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 rm .env
-echo "HOSTNAME=$HOSTNAME" >> .env
+echo "EXTERNALADDRESS=https://${HOSTNAME}/" >> .env
 echo "ADAGUC_HOME=$HOME" >> .env
 echo "ADAGUC_DATA_DIR=/data/adaguc-autowms" >> .env
 echo "ADAGUC_AUTOWMS_DIR=/data/adaguc-autowms" >> .env
 echo "ADAGUC_DATASET_DIR=/data/adaguc-datasets" >> .env
+echo "ADAGUC_PORT=443" >> .env
 
 
