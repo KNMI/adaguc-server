2c77fca70768be50817e1196830907701d5fe8bb
maartenplieger <maartenplieger@gmail.com>
2018-11-20 16:20:46 +0000

    Finishing touch on docker-compose

--- Docker/docker-compose-generate-env.sh
+++ Docker/docker-compose-generate-env.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 rm .env
+EXTERNALADDRESS="https://${HOSTNAME}/"
 echo "EXTERNALADDRESS=https://${HOSTNAME}/" >> .env
 echo "ADAGUC_HOME=$HOME" >> .env
 echo "ADAGUC_DATA_DIR=/data/adaguc-autowms" >> .env
@@ -7,4 +8,4 @@ echo "ADAGUC_AUTOWMS_DIR=/data/adaguc-autowms" >> .env
 echo "ADAGUC_DATASET_DIR=/data/adaguc-datasets" >> .env
 echo "ADAGUC_PORT=443" >> .env
 
-
+echo "ADAGUC will be available on ${EXTERNALADDRESS}"
