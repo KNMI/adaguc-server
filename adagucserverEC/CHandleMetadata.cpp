ed4d053326be49f4064be52ad00a0b69b274cd7d
Maarten Plieger <maartenplieger@gmail.com>
2019-07-22 16:27:08 +0200

    First steps to metadata handling

---
+++ adagucserverEC/CHandleMetadata.cpp
@@ -0,0 +1,9 @@
+
+#include "CHandleMetadata.h"
+const char *CHandleMetadata::className="CHandleMetadata";
+
+
+int CHandleMetadata::process() {
+    CDBDebug("OK");
+    return 0;
+}
---
+++ adagucserverEC/CHandleMetadata.h
@@ -0,0 +1,14 @@
+#ifndef CHandleMetadata_H
+#define CHandleMetadata_H
+#include "CDebugger.h"
+
+class CHandleMetadata {
+    private:
+    DEF_ERRORFUNCTION();
+  
+    public:
+    int process();
+
+};
+
+#endif
