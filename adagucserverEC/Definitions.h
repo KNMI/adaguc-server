0cb62a584b86abcf478e592cb6a77ba13256118e
Maarten Plieger <maartenplieger@gmail.com>
2020-01-14 10:24:02 +0100

    Version 2.2.7 support for radian

--- adagucserverEC/Definitions.h
+++ adagucserverEC/Definitions.h
@@ -29,7 +29,7 @@
 #ifndef Definitions_H
 #define Definitions_H
 
-#define ADAGUCSERVER_VERSION             "2.2.6"
+#define ADAGUCSERVER_VERSION             "2.2.7"
 
 //CConfigReaderLayerType
 #define CConfigReaderLayerTypeUnknown     0
