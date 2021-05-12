4c238e545afe01f79de55c93778cad21047c9c76
Maarten Plieger <maarten.plieger@knmi.nl>
2021-05-12 16:11:16 +0200

    Fixing projection issues with robinson by checking diagonal of quads

--- adagucserverEC/Definitions.h
+++ adagucserverEC/Definitions.h
@@ -28,7 +28,7 @@
 #ifndef Definitions_H
 #define Definitions_H
 
-#define ADAGUCSERVER_VERSION "2.5.10" // Please also update in the Dockerfile to the same version
+#define ADAGUCSERVER_VERSION "2.5.11" // Please also update in the Dockerfile to the same version
 
 //CConfigReaderLayerType
 #define CConfigReaderLayerTypeUnknown 0
