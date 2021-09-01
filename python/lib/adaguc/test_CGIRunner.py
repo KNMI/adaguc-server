da440195d417ffd5676bf0fbf747db517af65cda
Maarten Plieger <maarten.plieger@knmi.nl>
2021-09-01 14:26:26 +0200

    First routes are there

---
+++ python/lib/adaguc/test_CGIRunner.py
@@ -0,0 +1,50 @@
+import math
+import time
+
+#  cd  ./python/lib/adaguc/
+# pytest -rpP
+
+from .CGIRunner import CGIRunner
+from io import BytesIO
+
+def testCGIResponse80px():
+
+   filetogenerate = BytesIO();
+   cgiRunner = CGIRunner()
+   start = time.perf_counter()
+   status, headers = cgiRunner.run(["python3","mockCGIResponse.py", "mockcgidata_headersandcontent80px.bin"],"",output = filetogenerate,env=[])
+   extractedPng = filetogenerate.getvalue()
+   end = time.perf_counter()
+   print ("Seconds for 80px: %f" % (end - start))
+   #with open("mockcgidata_out80px.png", "wb") as f:
+   #   f.write(filetogenerate.getbuffer())
+
+   assert status == 0
+   assert headers == ['Content-Type:image/png']
+
+   with open('mockcgidata_out80px.png', mode='rb') as file: 
+    expectedPng = file.read()
+
+   assert extractedPng== expectedPng
+   
+def testCGIResponse8000px():
+    
+   filetogenerate = BytesIO();
+   cgiRunner = CGIRunner()
+   start = time.perf_counter()
+   status, headers = cgiRunner.run(["python3","mockCGIResponse.py", "mockcgidata_headersandcontent8000px.bin"],"",output = filetogenerate,env=[])
+   extractedPng = filetogenerate.getvalue()
+   end = time.perf_counter()
+   print ("Seconds for 8000px: %f" % (end - start))
+   #with open("mockcgidata_out8000px.png", "wb") as f:
+   #   f.write(filetogenerate.getbuffer())
+
+   assert status == 0
+   assert headers == ['Content-Type:image/png']
+
+   with open('mockcgidata_out8000px.png', mode='rb') as file: 
+    expectedPng = file.read()
+
+   assert extractedPng == expectedPng
+   
+
--- python/python-adaguc-server/main.py
+++ python/python-adaguc-server/main.py
@@ -7,11 +7,15 @@ configureLogging(logging)
 from routeAdagucServer import routeAdagucServer
 from routeAutoWMS import routeAutoWMS
 from routeAdagucOpenDAP import routeAdagucOpenDAP
+from routeHealthCheck import routeHealthCheck
+from routeRoot import routeRoot
 
 app = Flask(__name__)
 app.register_blueprint(routeAdagucServer)
 app.register_blueprint(routeAutoWMS)
 app.register_blueprint(routeAdagucOpenDAP)
+app.register_blueprint(routeRoot)
+
 
 
 if __name__ == "__main__":
