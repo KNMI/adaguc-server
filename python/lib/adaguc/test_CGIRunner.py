2bd91985b6d5c1d0cc1b617ded519379f2573110
Maarten Plieger <maarten.plieger@knmi.nl>
2021-10-14 17:58:24 +0200

    Resolved comments
    Former-commit-id: 3554e197cd0b4c1739f5c228d8f2ac303bff6c0f

--- python/lib/adaguc/test_CGIRunner.py
+++ python/lib/adaguc/test_CGIRunner.py
@@ -4,47 +4,49 @@ import time
 #  cd  ./python/lib/adaguc/
 # pytest -rpP
 
-from .CGIRunner import CGIRunner
+from .CGIRunner import CGIRunner as CGIRunner
 from io import BytesIO
+
 
 def testCGIResponse80px():
 
-   filetogenerate = BytesIO();
-   cgiRunner = CGIRunner()
-   start = time.perf_counter()
-   status, headers = cgiRunner.run(["python3","mockCGIResponse.py", "mockcgidata_headersandcontent80px.bin"],"",output = filetogenerate,env=[])
-   extractedPng = filetogenerate.getvalue()
-   end = time.perf_counter()
-   print ("Seconds for 80px: %f" % (end - start))
-   #with open("mockcgidata_out80px.png", "wb") as f:
-   #   f.write(filetogenerate.getbuffer())
+  filetogenerate = BytesIO()
+  cgiRunner = CGIRunner()
+  start = time.perf_counter()
+  status, headers = cgiRunner.run(
+      ["python3", "mockCGIResponse.py", "mockcgidata_headersandcontent80px.bin"], "", output=filetogenerate, env=[])
+  extractedPng = filetogenerate.getvalue()
+  end = time.perf_counter()
+  print("Seconds for 80px: %f" % (end - start))
+  # with open("mockcgidata_out80px.png", "wb") as f:
+  #   f.write(filetogenerate.getbuffer())
 
-   assert status == 0
-   assert headers == ['Content-Type:image/png']
+  assert status == 0
+  assert headers == ['Content-Type:image/png']
 
-   with open('mockcgidata_out80px.png', mode='rb') as file: 
+  with open('mockcgidata_out80px.png', mode='rb') as file:
     expectedPng = file.read()
 
-   assert extractedPng== expectedPng
-   
+  assert extractedPng == expectedPng
+
+
 def testCGIResponse8000px():
-    
-   filetogenerate = BytesIO();
-   cgiRunner = CGIRunner()
-   start = time.perf_counter()
-   status, headers = cgiRunner.run(["python3","mockCGIResponse.py", "mockcgidata_headersandcontent8000px.bin"],"",output = filetogenerate,env=[])
-   extractedPng = filetogenerate.getvalue()
-   end = time.perf_counter()
-   print ("Seconds for 8000px: %f" % (end - start))
-   #with open("mockcgidata_out8000px.png", "wb") as f:
-   #   f.write(filetogenerate.getbuffer())
 
-   assert status == 0
-   assert headers == ['Content-Type:image/png']
+  filetogenerate = BytesIO()
+  cgiRunner = CGIRunner()
+  start = time.perf_counter()
+  status, headers = cgiRunner.run(
+      ["python3", "mockCGIResponse.py", "mockcgidata_headersandcontent8000px.bin"], "", output=filetogenerate, env=[])
+  extractedPng = filetogenerate.getvalue()
+  end = time.perf_counter()
+  print("Seconds for 8000px: %f" % (end - start))
+  # with open("mockcgidata_out8000px.png", "wb") as f:
+  #   f.write(filetogenerate.getbuffer())
 
-   with open('mockcgidata_out8000px.png', mode='rb') as file: 
+  assert status == 0
+  assert headers == ['Content-Type:image/png']
+
+  with open('mockcgidata_out8000px.png', mode='rb') as file:
     expectedPng = file.read()
 
-   assert extractedPng == expectedPng
-   
-
+  assert extractedPng == expectedPng
