70c24707f9a6aff9639fffe30a30f831ab4c1754
Maarten Plieger <maarten.plieger@knmi.nl>
2021-10-14 16:08:28 +0200

    updated doc and added startup check

--- python/python-adaguc-server/main.py
+++ python/python-adaguc-server/main.py
@@ -17,8 +17,27 @@ app.register_blueprint(routeAdagucOpenDAP)
 app.register_blueprint(routeRoot)
 app.register_blueprint(routeHealthCheck)
 
+from setupAdaguc import setupAdaguc
+
+def testadaguc():
+    print("Checking adaguc-server.")
+    adagucInstance = setupAdaguc()
+    url = "SERVICE=WMS&REQUEST=GETCAPABILITIES"
+    adagucenv={}
+
+    """ Set required environment variables """
+    baseUrl = "---"
+    adagucenv['ADAGUC_ONLINERESOURCE']=os.getenv('EXTERNALADDRESS', baseUrl) + "/adaguc-server?"
+    adagucenv['ADAGUC_DB']=os.getenv('ADAGUC_DB', "user=adaguc password=adaguc host=localhost dbname=adaguc")
+    
+    """ Run adaguc-server """
+    status,data,headers = adagucInstance.runADAGUCServer(url, env = adagucenv,  showLogOnError = False)
+    assert status == 0
+    assert headers == ['Content-Type:text/xml']
+    print("[OK]")
 
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
+    testadaguc()
     app.run(debug=True, host="0.0.0.0", port=8080)
