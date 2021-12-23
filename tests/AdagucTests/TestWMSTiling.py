40203cae6063981afebaa63e941811b4990ae78c
Maarten Plieger <maarten.plieger@knmi.nl>
2021-12-23 15:19:17 +0100

    Added extra tests for tiling

--- tests/AdagucTests/TestWMSTiling.py
+++ tests/AdagucTests/TestWMSTiling.py
@@ -1,6 +1,6 @@
-#pylint: disable=line-too-long
-#pylint: disable=unused-variable
-#pylint: disable=invalid-name
+# pylint: disable=line-too-long
+# pylint: disable=unused-variable
+# pylint: disable=invalid-name
 
 """
   Run test for tiling system of adaguc-server
@@ -24,9 +24,31 @@ class TestWMSTiling(unittest.TestCase):
            "/data/config/adaguc.tests.dataset.xml"}
     AdagucTestTools().mkdir_p(testresultspath)
 
-    def test_WMSGetMap_testdatanc(self):
+    def test_WMSGetMap_testdatanc_notiling(self):
         """
-        Testing tiling
+        Testing standard functionality without tiling
+        """
+        AdagucTestTools().cleanTempDir()
+
+        config = ADAGUC_PATH + '/data/config/adaguc.tests.dataset.xml,' + \
+            ADAGUC_PATH + '/data/config/datasets/adaguc.testtiling.xml'
+
+        status, data, headers = AdagucTestTools().runADAGUCServer(
+            args=['--updatedb', '--config', config], env=self.env, isCGI=False)
+        self.assertEqual(status, 0)
+
+        filename = "test_TestWMSTilingWMSGetMap_testdatanc-notiling.png"
+
+        status, data, headers = AdagucTestTools().runADAGUCServer(
+            "dataset=adaguc.testtiling&SERVICE=WMS&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=testdatant&WIDTH=256&HEIGHT=180&CRS=EPSG%3A4326&BBOX=-90,-180,90,180&STYLES=testdata%2Fnearest&FORMAT=image/png&TRANSPARENT=FALSE&", env=self.env, showLogOnError=True, showLog=False)
+        AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
+        self.assertEqual(status, 0)
+        self.assertEqual(data.getvalue(), AdagucTestTools(
+        ).readfromfile(self.expectedoutputsspath + filename))
+
+    def test_WMSGetMap_testdatanc_tiling(self):
+        """
+        Testing tiling using the createtiles command
         """
         AdagucTestTools().cleanTempDir()
 
@@ -56,6 +78,28 @@ class TestWMSTiling(unittest.TestCase):
         filename = "test_TestWMSTilingWMSGetMap_testdatanc.png"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "dataset=adaguc.testtiling&SERVICE=WMS&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=testdata&WIDTH=256&HEIGHT=180&CRS=EPSG%3A4326&BBOX=-90,-180,90,180&STYLES=testdata%2Fnearest&FORMAT=image/png&TRANSPARENT=FALSE&", env=self.env, showLogOnError=True, showLog=False)
+        AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
+        self.assertEqual(status, 0)
+        self.assertEqual(data.getvalue(), AdagucTestTools(
+        ).readfromfile(self.expectedoutputsspath + filename))
+
+    def test_WMSGetMap_testdatanc_autotiling(self):
+        """
+        Testing auto tiling, tiling done during --updatedb
+        """
+        AdagucTestTools().cleanTempDir()
+
+        config = ADAGUC_PATH + '/data/config/adaguc.tests.dataset.xml,' + \
+            ADAGUC_PATH + '/data/config/datasets/adaguc.testautotiling.xml'
+
+        status, data, headers = AdagucTestTools().runADAGUCServer(
+            args=['--updatedb', '--config', config], env=self.env, isCGI=False)
+        self.assertEqual(status, 0)
+
+        filename = "test_TestWMSTilingWMSGetMap_testdatanc-autotiling.png"
+
+        status, data, headers = AdagucTestTools().runADAGUCServer(
+            "dataset=adaguc.testautotiling&SERVICE=WMS&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=testdata&WIDTH=256&HEIGHT=180&CRS=EPSG%3A4326&BBOX=-90,-180,90,180&STYLES=testdata%2Fnearest&FORMAT=image/png&TRANSPARENT=FALSE&", env=self.env, showLogOnError=True, showLog=False)
         AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
         self.assertEqual(status, 0)
         self.assertEqual(data.getvalue(), AdagucTestTools(
