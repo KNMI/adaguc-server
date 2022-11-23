e3c46e8604dce47ba8e7e146263e2e10ca9c36a7
Maarten Plieger <maarten.plieger@knmi.nl>
2022-11-23 09:26:32 +0100

    Adding ODIM test

--- tests/AdagucTests/TestWMS.py
+++ tests/AdagucTests/TestWMS.py
@@ -1064,5 +1064,25 @@ class TestWMS(unittest.TestCase):
         self.assertTrue(AdagucTestTools().compareGetCapabilitiesXML(
             self.testresultspath + filename, self.expectedoutputsspath + filename))
         self.assertEqual(status, 0)
+    
+    def test_WMSGetMap_ODIMHDF5_RAD_CU21_PPZ_E05(self):
+        AdagucTestTools().cleanTempDir()
+        config = ADAGUC_PATH + '/data/config/adaguc.tests.dataset.xml,' + \
+            ADAGUC_PATH + '/data/config/datasets/adaguc.tests.ODIMHDF5.xml'
+        env = {'ADAGUC_CONFIG': config}
+        # pylint: disable=unused-variable
+        status, data, headers = AdagucTestTools().runADAGUCServer(
+            args=['--updatedb', '--config', config], env=self.env, isCGI=False)
+        self.assertEqual(status, 0)
+
+        filename = "test_WMSGetMap_ODIMHDF5_RAD_CU21_PPZ_E05.png"
+        status, data, headers = AdagucTestTools().runADAGUCServer(
+            "DATASET=adaguc.tests.ODIMHDF5&SERVICE=WMS&&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=CAPPI&WIDTH=746&HEIGHT=927&CRS=EPSG%3A3857&BBOX=-8153802.59473168,773907.3916620318,-7198456.3109903205,1961046.808589968&STYLES=radar%2Fnearest&FORMAT=image/png&TRANSPARENT=TRUE&&time=2022-08-30T22%3A35%3A00Z", env=env)
+        AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
+        self.assertEqual(status, 0)
+        self.assertEqual(data.getvalue(), AdagucTestTools(
+        ).readfromfile(self.expectedoutputsspath + filename))
+
+
 
 
