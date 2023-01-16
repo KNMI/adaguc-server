f18cbb4a83245d9f4084a0437887538486d6a318
Ernst de Vreede <ernst.de.vreede@knmi.nl>
2023-01-16 17:26:46 +0100

    Fixed testtools

--- python/lib/adaguc/AdagucTestTools.py
+++ python/lib/adaguc/AdagucTestTools.py
@@ -6,12 +6,12 @@ import re
 from lxml import etree, objectify
 import urllib.request
 
-ADAGUC_PATH = os.getenv('ADAGUC_PATH', " ")
+ADAGUC_PATH = os.getenv("ADAGUC_PATH", " ")
 
 
 class AdagucTestTools:
     def getLogFile(self):
-        ADAGUC_LOGFILE = os.environ['ADAGUC_LOGFILE']
+        ADAGUC_LOGFILE = os.environ["ADAGUC_LOGFILE"]
         try:
             f = open(ADAGUC_LOGFILE, encoding="utf-8")
             data = f.read()
@@ -20,9 +20,9 @@ class AdagucTestTools:
         except Exception:
             pass
         return ""
-    
+
     def getLogFileSize(self):
-        ADAGUC_LOGFILE = os.environ['ADAGUC_LOGFILE']
+        ADAGUC_LOGFILE = os.environ["ADAGUC_LOGFILE"]
         file_stats = os.stat(ADAGUC_LOGFILE)
         return file_stats.st_size
 
@@ -36,7 +36,17 @@ class AdagucTestTools:
         content = urllib.request.urlopen(req)
         return [content.getcode(), content.read(), content.getheaders()]
 
-    def runADAGUCServer(self, url=None, env=None, path=None, args=None, isCGI=True, showLogOnError=True, showLog=False, maxLogFileSize = 8192):
+    def runADAGUCServer(
+        self,
+        url=None,
+        env=None,
+        path=None,
+        args=None,
+        isCGI=True,
+        showLogOnError=True,
+        showLog=False,
+        maxLogFileSize=8192,
+    ):
 
         if env is None:
             env = []
@@ -44,25 +54,31 @@ class AdagucTestTools:
         adagucenv = os.environ.copy()
         adagucenv.update(env)
 
-        ADAGUC_LOGFILE = os.environ['ADAGUC_LOGFILE']
+        ADAGUC_LOGFILE = os.environ["ADAGUC_LOGFILE"]
 
         try:
             os.remove(ADAGUC_LOGFILE)
         except Exception:
             pass
 
-        adagucexecutable = ADAGUC_PATH+"/bin/adagucserver"
+        adagucexecutable = ADAGUC_PATH + "/bin/adagucserver"
 
         adagucargs = [adagucexecutable]
 
         if args is not None:
             adagucargs = adagucargs + args
 
-        os.chdir(ADAGUC_PATH+"/tests")
+        os.chdir(ADAGUC_PATH + "/tests")
 
         filetogenerate = BytesIO()
-        status, headers, processErr = CGIRunner().run(adagucargs, url=url, output=filetogenerate,
-                                          env=adagucenv, path=path, isCGI=isCGI)
+        status, headers, processErr = CGIRunner().run(
+            adagucargs,
+            url=url,
+            output=filetogenerate,
+            env=adagucenv,
+            path=path,
+            isCGI=isCGI,
+        )
 
         if (status != 0 and showLogOnError == True) or showLog == True:
             print("\n\n--- START ADAGUC DEBUG INFO ---")
@@ -89,28 +105,31 @@ class AdagucTestTools:
         else:
             # The executable wrote to stderr, which is unwanted behaviour. Stderr should be empty when running adaguc-server.
             if processErr:
-                #print(processErr.decode())  
-                print("[WARNING]: the process should not write to stderr")    
+                # print(processErr.decode())
+                print("[WARNING]: the process should not write to stderr")
             # Check if the code did not write a too big logfile
             logfileSize = self.getLogFileSize()
             if logfileSize > maxLogFileSize:
                 self.printLogFile()
-                print("[WARNING]: Adaguc-server writes too many lines to the logfile, size = %d kilobytes" % (logfileSize/1024))
+                print(
+                    "[WARNING]: Adaguc-server writes too many lines to the logfile, size = %d kilobytes"
+                    % (logfileSize / 1024)
+                )
             return [status, filetogenerate, headers]
 
     def writetofile(self, filename, data):
-        with open(filename, 'wb') as f:
+        with open(filename, "wb") as f:
             f.write(data)
 
     def readfromfile(self, filename):
-        adagucpath = os.getenv('ADAGUC_PATH')
+        adagucpath = os.getenv("ADAGUC_PATH")
         if adagucpath:
             filename = filename.replace("{ADAGUC_PATH}/", adagucpath)
-        with open(filename, 'rb') as f:
+        with open(filename, "rb") as f:
             return f.read()
 
     def cleanTempDir(self):
-        ADAGUC_TMP = os.getenv('ADAGUC_TMP', 'adaguctmp')
+        ADAGUC_TMP = os.getenv("ADAGUC_TMP", "adaguctmp")
         try:
             shutil.rmtree(ADAGUC_TMP)
         except Exception:
@@ -122,14 +141,16 @@ class AdagucTestTools:
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-    def compareGetCapabilitiesXML(self, testresultFileLocation, expectedOutputFileLocation):
+    def compareGetCapabilitiesXML(
+        self, testresultFileLocation, expectedOutputFileLocation
+    ):
         expectedxml = self.readfromfile(expectedOutputFileLocation)
         testxml = self.readfromfile(testresultFileLocation)
 
         obj1 = objectify.fromstring(
-            re.sub(b' xmlns="[^"]+"', b'', expectedxml, count=1))
-        obj2 = objectify.fromstring(
-            re.sub(b' xmlns="[^"]+"', b'', testxml, count=1))
+            re.sub(b' xmlns="[^"]+"', b"", expectedxml, count=1)
+        )
+        obj2 = objectify.fromstring(re.sub(b' xmlns="[^"]+"', b"", testxml, count=1))
 
         # Remove ADAGUC build date and version from keywordlists
         try:
@@ -144,6 +165,17 @@ class AdagucTestTools:
                 child.getparent().remove(child)
             for child in obj2.findall("Service/ServerInfo")[0]:
                 child.getparent().remove(child)
+        except Exception:
+            pass
+
+        # Remove ContactInformation if it is empty element
+        try:
+            for child in obj1.findall("Service/ContactInformation")[0]:
+                if child.text.strip() == "":
+                    child.getparent().remove(child)
+            for child in obj2.findall("Service/ContactInformation")[0]:
+                if child.text.strip() == "":
+                    child.getparent().remove(child)
         except Exception:
             pass
 
@@ -168,7 +200,9 @@ class AdagucTestTools:
         expect = etree.tostring(obj2)
 
         if (result == expect) is False:
-            print("\nExpected XML is different, file \n\"%s\"\n should be equal to \n\"%s\"" % (
-                testresultFileLocation, expectedOutputFileLocation))
+            print(
+                '\nExpected XML is different, file \n"%s"\n should be equal to \n"%s"'
+                % (testresultFileLocation, expectedOutputFileLocation)
+            )
 
         return result == expect
