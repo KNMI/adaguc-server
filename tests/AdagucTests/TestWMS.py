8aeea37b0661aa743be34a36cca058674883d530
Lukas Phaf <lukas.phaf@knmi.nl>
2023-01-25 16:38:24 +0100

    Revert "Add ImageCompare to tests that break on github docker build."
    This reverts commit 0706f97ae7e53426fdc6f4b8fd98b4299575d175 [formerly 314ac6dfe9ef9924cadfba8e24687b4d25cea0f6].
    
    
    Former-commit-id: aa13e00b00f803aecafa184ab875098b16b9c05d

--- tests/AdagucTests/TestWMS.py
+++ tests/AdagucTests/TestWMS.py
@@ -548,7 +548,8 @@ class TestWMS(unittest.TestCase):
                                                                   {'ADAGUC_CONFIG': ADAGUC_PATH + '/data/config/adaguc.tests.autostyle.xml'})
         AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
         self.assertEqual(status, 0)
-        self.assertTrue(AdagucTestTools().compareImage(self.expectedoutputsspath + filename, self.testresultspath + filename))
+        self.assertEqual(data.getvalue(), AdagucTestTools(
+        ).readfromfile(self.expectedoutputsspath + filename))
 
     def test_WMSGetMapWithManyContourDefinitions_testdatanc(self):
         AdagucTestTools().cleanTempDir()
@@ -576,7 +577,8 @@ class TestWMS(unittest.TestCase):
                                                                   {'ADAGUC_CONFIG': ADAGUC_PATH + '/data/config/adaguc.tests.autostyle.xml'})
         AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
         self.assertEqual(status, 0)
-        self.assertTrue(AdagucTestTools().compareImage(self.expectedoutputsspath + filename, self.testresultspath + filename))
+        self.assertEqual(data.getvalue(), AdagucTestTools(
+        ).readfromfile(self.expectedoutputsspath + filename))
 
     def test_WMSGetMapWithShowLegendNothing_testdatanc(self):
         AdagucTestTools().cleanTempDir()
@@ -590,7 +592,8 @@ class TestWMS(unittest.TestCase):
                                                                   {'ADAGUC_CONFIG': ADAGUC_PATH + '/data/config/adaguc.tests.autostyle.xml'})
         AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
         self.assertEqual(status, 0)
-        self.assertTrue(AdagucTestTools().compareImage(self.expectedoutputsspath + filename, self.testresultspath + filename))
+        self.assertEqual(data.getvalue(), AdagucTestTools(
+        ).readfromfile(self.expectedoutputsspath + filename))
 
     def test_WMSGetMapWithShowLegendSecondLayer_testdatanc(self):
         AdagucTestTools().cleanTempDir()
@@ -604,7 +607,8 @@ class TestWMS(unittest.TestCase):
                                                                   {'ADAGUC_CONFIG': ADAGUC_PATH + '/data/config/adaguc.tests.autostyle.xml'})
         AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
         self.assertEqual(status, 0)
-        self.assertTrue(AdagucTestTools().compareImage(self.expectedoutputsspath + filename, self.testresultspath + filename))
+        self.assertEqual(data.getvalue(), AdagucTestTools(
+        ).readfromfile(self.expectedoutputsspath + filename))
 
     def test_WMSGetMapWithShowLegendAllLayers_testdatanc(self):
         AdagucTestTools().cleanTempDir()
@@ -618,7 +622,8 @@ class TestWMS(unittest.TestCase):
                                                                   {'ADAGUC_CONFIG': ADAGUC_PATH + '/data/config/adaguc.tests.autostyle.xml'})
         AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
         self.assertEqual(status, 0)
-        self.assertTrue(AdagucTestTools().compareImage(self.expectedoutputsspath + filename, self.testresultspath + filename))
+        self.assertEqual(data.getvalue(), AdagucTestTools(
+        ).readfromfile(self.expectedoutputsspath + filename))
 
     def test_WMSGetMapRobinsonProjection_sample_tas_cmip6_ssp585_preIndustrial_warming2_year(self):
         AdagucTestTools().cleanTempDir()
