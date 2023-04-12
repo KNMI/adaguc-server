6a3fef84b6bcf036b84c5da9dacc665d2602f259
Lukas Phaf <lukas.phaf@knmi.nl>
2023-04-12 17:29:54 +0200

    Remove PROJ_IGNORE_CELESTIAL_BODY, no longer needed with other fixes.
    Former-commit-id: 32a31840bcd42682fc873b5521b943bd2e6fec61

--- python/lib/adaguc/AdagucTestTools.py
+++ python/lib/adaguc/AdagucTestTools.py
@@ -43,7 +43,6 @@ class AdagucTestTools:
             env = []
 
         adagucenv = os.environ.copy()
-        adagucenv['PROJ_IGNORE_CELESTIAL_BODY'] = 'YES'  # Needed because KNMI proj strings have +a and +b in km, not meters
         adagucenv.update(env)
 
         ADAGUC_LOGFILE = os.environ['ADAGUC_LOGFILE']
