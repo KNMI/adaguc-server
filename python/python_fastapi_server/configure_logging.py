f6ec5096b20d662dfa03364759360fdd6d7a463c
maarten.plieger <maarten.plieger@knmi.nl>
2023-06-14 09:49:27 +0200

    Disabled double or threedouble logging and added timeout of 60 seconds
    Former-commit-id: 94e13f176ee32350763bfa7a72e3d05d9a396979

--- python/python_fastapi_server/configure_logging.py
+++ python/python_fastapi_server/configure_logging.py
@@ -1,9 +1,20 @@
 # pylint: disable=invalid-name
-
 """Configures logging for the adaguc-server python wrapper"""
 import sys
 
 
+def run_once(f):
+
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f(*args, **kwargs)
+
+    wrapper.has_run = False
+    return wrapper
+
+
+@run_once
 def configure_logging(logging):
     """Configures logging for the adaguc-server python wrapper"""
     root = logging.getLogger()
@@ -11,7 +22,6 @@ def configure_logging(logging):
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
-        "applicationlog %(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
+        "applicationlog %(asctime)s - %(name)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     root.addHandler(handler)
