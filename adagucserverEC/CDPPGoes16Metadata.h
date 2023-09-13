adbf61432584e0f346372ff1ce8838e866886b4c
Maarten Plieger <maarten.plieger@knmi.nl>
2023-09-13 17:20:18 +0200

    Update adagucserverEC/CDPPGoes16Metadata.h
    Co-authored-by: Lukas Phaf <lukas.phaf@knmi.nl>

--- adagucserverEC/CDPPGoes16Metadata.h
+++ adagucserverEC/CDPPGoes16Metadata.h
@@ -11,7 +11,7 @@ private:
 
 public:
   virtual const char *getId();
-  virtual int isApplicable(CServerConfig::XMLE_DataPostProc *proc, CDataSource *dataSource, int);
+  virtual int isApplicable(CServerConfig::XMLE_DataPostProc *proc, CDataSource *dataSource, int mode);
   virtual int execute(CServerConfig::XMLE_DataPostProc *proc, CDataSource *dataSource, int mode);
   virtual int execute(CServerConfig::XMLE_DataPostProc *, CDataSource *, int, double *, size_t) { return 1; } // TODO: Still need to implement
 };
