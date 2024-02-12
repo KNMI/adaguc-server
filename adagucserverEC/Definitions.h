ad96f73b3abf4c95c97f28187673ed43b308cd30
Ernst de Vreede <ernst.de.vreede@knmi.nl>
2024-02-12 08:50:04 +0100

    Version bump

--- adagucserverEC/Definitions.h
+++ adagucserverEC/Definitions.h
@@ -28,7 +28,7 @@
 #ifndef Definitions_H
 #define Definitions_H
 
-#define ADAGUCSERVER_VERSION "2.16.0" // Please also update in the Dockerfile to the same version
+#define ADAGUCSERVER_VERSION "2.16.1" // Please also update in the Dockerfile to the same version
 
 // CConfigReaderLayerType
 #define CConfigReaderLayerTypeUnknown 0
