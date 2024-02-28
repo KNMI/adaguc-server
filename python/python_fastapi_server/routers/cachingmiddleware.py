31dc3edc23c8f9d40711138e97369f7a2708ac96
Ernst de Vreede <ernst.de.vreede@knmi.nl>
2024-02-28 17:57:30 +0100

    worked on review comments

--- python/python_fastapi_server/routers/cachingmiddleware.py
+++ python/python_fastapi_server/routers/cachingmiddleware.py
@@ -1,14 +1,12 @@
 import os
 from urllib.parse import urlsplit
 
-# from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import Response
 from fastapi import BackgroundTasks
 
 import calendar
-from datetime import datetime, timedelta
-import time
+from datetime import datetime
 import redis.asyncio as redis
 import json
 
@@ -17,36 +15,35 @@ ADAGUC_REDIS=os.environ.get('ADAGUC_REDIS')
 async def get_cached_response(redis_client, request):
     key = generate_key(request)
     cached = await redis_client.get(key)
-    await redis_client.aclose()
+    # await redis_client.close()
     if not cached:
         # print("Cache miss")
         return None, None, None
     # print("Cache hit", len(cached))
 
-    entrytime=int(cached[:10])
-    currenttime=calendar.timegm(datetime.utcnow().utctimetuple())
-    age=currenttime-entrytime
+    entrytime = int(cached[:10])
+    currenttime = calendar.timegm(datetime.utcnow().utctimetuple())
+    age = currenttime-entrytime
 
-    headers_len=int(cached[10:16])
-    headers=json.loads(cached[16:16+headers_len])
+    headers_len = int(cached[10:16])
+    headers = json.loads(cached[16:16+headers_len])
 
     data = cached[16+headers_len:]
     return age, headers, data
 
-skip_headers=["x-process-time", "age"]
+skip_headers = ["x-process-time", "age"]
 
 async def cache_response(redis_client, request, headers, data, ex: int=60):
     key=generate_key(request)
 
-    allheaders={}
+    headers_to_keep={}
     for k in headers.keys():
         if k not in skip_headers:
-            allheaders[k]=headers[k]
-    allheaders_json=json.dumps(allheaders)
+            headers_to_keep[k] = headers[k]
+    headers_to_keep_json = json.dumps(headers_to_keep)
 
     entrytime="%10d"%calendar.timegm(datetime.utcnow().utctimetuple())
-    await redis_client.set(key, bytes(entrytime, 'utf-8')+bytes("%06d"%len(allheaders_json), 'utf-8')+bytes(allheaders_json, 'utf-8')+data, ex=ex)
-    await redis_client.aclose()
+    await redis_client.set(key, bytes(entrytime, 'utf-8')+bytes("%06d"%len(headers_to_keep_json), 'utf-8')+bytes(headers_to_keep_json, 'utf-8')+data, ex=ex)
 
 def generate_key(request):
     key = f"{request.url.path}?{request['query_string']}"
@@ -59,21 +56,18 @@ class CachingMiddleware(BaseHTTPMiddleware):
         super().__init__(app)
         if "ADAGUC_REDIS" in os.environ:
             self.shortcut=False
-            self.redis = None
-        self.shortcut = True #TODO
+            self.redis = redis.from_url(ADAGUC_REDIS)
 
     async def dispatch(self, request, call_next):
-        if self.redis is None:
-            self.redis = redis.from_url(ADAGUC_REDIS)
         if self.shortcut:
             return await call_next(request)
 
         #Check if request is in cache, if so return that
-        expire, headers, data = await get_cached_response(self.redis, request)
+        age, headers, data = await get_cached_response(self.redis, request)
 
         if data:
             #Fix Age header
-            headers["Age"]="%1d"%(expire)
+            headers["Age"] = "%1d"%(age)
             return Response(content=data, status_code=200, headers=headers, media_type=headers['content-type'])
 
         response: Response = await call_next(request)
@@ -85,15 +79,15 @@ class CachingMiddleware(BaseHTTPMiddleware):
                 for term in cache_control_terms:
                     age_terms = term.split("=")
                     if age_terms[0].lower()=="max-age":
-                        ttl=int(age_terms[1])
+                        ttl = int(age_terms[1])
                         break
                 if ttl is None:
-                    return
+                    return response
                 response_body = b""
                 async for chunk in response.body_iterator:
-                    response_body+=chunk
+                    response_body += chunk
                 tasks = BackgroundTasks()
                 tasks.add_task(cache_response, redis_client=self.redis, request=request, headers=response.headers, data=response_body, ex=ttl)
-                response.headers['age']="0"
+                response.headers['age'] = "0"
                 return Response(content=response_body, status_code=200, headers=response.headers, media_type=response.media_type, background=tasks)
         return response
