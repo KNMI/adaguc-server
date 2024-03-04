a816233e36ee56e030eee983f21ead878d8c7aa2
Matthijs Grünbauer <matthijs.grunbauer@knmi.nl>
2024-03-04 10:13:21 +0100

    Add PGBOUNCE_DB env

--- run_supervisord.sh
+++ run_supervisord.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Look up db user/pass/hostname/dbname from ADAGUC_DB string
+# Look up db host/port/user/password/name from PGBOUNCE_DB string
+# $PGBOUNCE_DB has the same format as $ADAGUC_DB
 # Use these values to setup pgbouncer when running in AWS
-db_host=$(echo $ADAGUC_DB | cut -d' ' -f1 | cut -d'=' -f2)
-db_port=$(echo $ADAGUC_DB | cut -d' ' -f2 | cut -d'=' -f2)
-db_user=$(echo $ADAGUC_DB | cut -d' ' -f3 | cut -d'=' -f2)
-db_password=$(echo $ADAGUC_DB | cut -d' ' -f4 | cut -d'=' -f2)
-db_name=$(echo $ADAGUC_DB | cut -d' ' -f5 | cut -d'=' -f2)
+read -r db_host db_port db_user db_password db_name <<< $(echo $PGBOUNCE_DB | awk -F'[= ]' '{ print $2" "$4" "$6" "$8" "$10 }')
 
+# set user password
 echo '"'$db_user'"' '"'$db_password'"' > /adaguc/adaguc-server-master/userlist.txt
 
+# rewrite pgbouncer config
 pgbouncer_cfg="adaguc = host=$db_host port=$db_port dbname=$db_name"
 sed -i "s/^adaguc = .*/${pgbouncer_cfg}/g" /adaguc/adaguc-server-master/pgbouncer.ini
 
+# supervisord starts both pgbouncer and adaguc
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
