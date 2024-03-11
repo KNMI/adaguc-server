2a1cd6fe81e13261ebc87a893479de5feecaf676
Ernst de Vreede <ernst.de.vreede@knmi.nl>
2024-03-11 17:21:20 +0100

    Changed x-process-time header generation to include cache access time

--- python/python_fastapi_server/main.py
+++ python/python_fastapi_server/main.py
@@ -37,15 +37,6 @@ logging.getLogger("access").propagate = False
 
 
 @app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
-    return response
-
-
-@app.middleware("http")
 async def add_hsts_header(request: Request, call_next):
     response = await call_next(request)
     if "EXTERNALADDRESS" in os.environ:
@@ -73,6 +64,16 @@ if "ADAGUC_REDIS" in os.environ:
 
 if "EXTERNALADDRESS" in os.environ:
     app.add_middleware(FixSchemeMiddleware)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
 
 app.add_middleware(BrotliMiddleware, gzip_fallback=True)
 
