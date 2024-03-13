42094cd26ec9d5b9e3a70212c597ae3f46045e4a
maartenplieger <maarten.plieger@knmi.nl>
2024-03-13 11:07:39 +0100

    Less logging and force CORS header

--- python/python_fastapi_server/main.py
+++ python/python_fastapi_server/main.py
@@ -72,6 +72,8 @@ async def add_process_time_header(request: Request, call_next):
     response = await call_next(request)
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
+    # TODO: Temporary fix to enable CORS, it seems that CORSMiddleware does not always send this information
+    response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
 
