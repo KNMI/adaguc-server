9f4147cf88c236ce8e1cc02aae6a0f49f46d2faf
Maarten Plieger <maarten.plieger@knmi.nl>
2024-06-06 16:17:23 +0200

    Added tests for extra edr metadata

--- python/python_fastapi_server/test_ogc_api_edr.py
+++ python/python_fastapi_server/test_ogc_api_edr.py
@@ -71,6 +71,51 @@ def test_collections(client: TestClient):
 
     assert "position" in coll_5d["data_queries"]
 
+    assert "parameter_names" in coll_5d
+
+    parameter_names = coll_5d["parameter_names"]
+
+    assert "data" in parameter_names
+
+    data = parameter_names["data"]
+
+    assert data == {
+      "type": "Parameter",
+      "id": "data",
+      "label": "data",
+      "unit": {
+        "symbol": {
+          "value": "unit",
+          "type": "http://www.opengis.net/def/uom/UCUM"
+        }
+      },
+      "observedProperty": {
+        "id": "data",
+        "label": "data"
+      }
+    }
+
+
+    assert "data_extra_metadata" in parameter_names
+
+    data_extra_metadata = parameter_names["data_extra_metadata"]
+
+    assert data_extra_metadata == {
+      "type": "Parameter",
+      "id": "data_extra_metadata",
+      "label": "Air temperature, 2 metre",
+      "unit": {
+        "symbol": {
+          "value": "\u00b0C",
+          "type": "http://www.opengis.net/def/uom/UCUM"
+        }
+      },
+      "observedProperty": {
+        "id": "https://vocab.nerc.ac.uk/standard_name/air_temperature",
+        "label": "Air temperature"
+      }
+    }
+
 
 def test_coll_5d_position(client: TestClient):
     resp = client.get(
