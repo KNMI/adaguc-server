9878de2d4cd0e5d75f4bb9be9e662fc5365e1061
Matthijs Grünbauer <matthijs.grunbauer@knmi.nl>
2024-09-04 17:54:35 +0200

    Experiment with forking adaguc

--- adagucserverEC/adagucserver.cpp
+++ adagucserverEC/adagucserver.cpp
@@ -43,12 +43,13 @@
 #include "CXMLParser.h"
 #include "CStopWatch.h"
 #include "CServerError.h"
+#include "fork_server.h"
 
 int processQueryStringRequest() {
   /* Process the OGC request */
-  setErrorFunction(serverErrorFunction);
-  setWarningFunction(serverWarningFunction);
-  setDebugFunction(serverDebugFunction);
+  // setErrorFunction(serverErrorFunction);
+  // setWarningFunction(serverWarningFunction);
+  // setDebugFunction(serverDebugFunction);
   CRequest request;
   if (setCRequestConfigFromEnvironment(&request) != 0) {
     CDBError("Unable to read configuration file.");
@@ -58,7 +59,7 @@ int processQueryStringRequest() {
   return getStatusCode();
 }
 
-int main(int argc, char **argv, char **envp) {
+int run_adaguc_once(int argc, char **argv, char **envp, bool is_forked) {
 
   StopWatch_Start();
 
@@ -92,4 +93,19 @@ int main(int argc, char **argv, char **envp) {
   closeLogFile();
 
   return status;
+}
+
+int main(int argc, char **argv, char **envp) {
+  // If these lines are commented out, the calls the /edr/collections/instances/<my-instance> fail to return data
+  // because the call to `request=getreferencetimes` does not contain useful output
+  setvbuf(stdout, NULL, _IONBF, 0); // turn off buffering
+  setvbuf(stderr, NULL, _IONBF, 0); // turn off buffering
+
+  const char *ADAGUC_FORK_SOCKET_PATH = getenv("ADAGUC_FORK_SOCKET_PATH");
+  if (ADAGUC_FORK_SOCKET_PATH != NULL) {
+    return run_as_fork_service(run_adaguc_once, argc, argv, envp);
+  } else {
+    // normal flow without unix socket server/fork
+    return run_adaguc_once(argc, argv, envp, false);
+  }
 }
