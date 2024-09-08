8b0707d3a3c64cd5ec1c759c4caf6f746c7b4a89
Matthijs Grünbauer <matthijs.grunbauer@knmi.nl>
2024-09-08 20:21:01 +0200

    Ignore child signal to stop zombies

--- adagucserverEC/adagucserver.cpp
+++ adagucserverEC/adagucserver.cpp
@@ -42,6 +42,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <stdio.h>
+#include <signal.h>
 
 DEF_ERRORMAIN();
 
@@ -488,6 +489,8 @@ void handle_client(int client_socket, int argc, char **argv, char **envp) {
 
 int run_server(int argc, char **argv, char **envp) {
   int client_socket = 0;
+
+  signal(SIGCHLD, SIG_IGN);
 
   struct sockaddr_un local, remote;
   int len = 0;
