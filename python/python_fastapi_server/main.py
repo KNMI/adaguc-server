80da27dfb5b3f66cac5184d36cdb636ea556ae40
Maarten Plieger <maarten.plieger@knmi.nl>
2024-10-17 17:17:58 +0200

    Fix error is bound to a different event loop

--- python/python_fastapi_server/main.py
+++ python/python_fastapi_server/main.py
@@ -1,4 +1,5 @@
 """Main file where FastAPI is defined and started"""
+import asyncio
 import logging
 import os
 import time
@@ -41,8 +42,8 @@ async def lifespan(_fastapiapp: FastAPI):
 
     logger.info("=== Starting AsyncIO Scheduler ===")
     # start scheduler to refresh collections & docs every minute
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(update_layermetadatatable, "cron", [], minute="*", jitter=0, max_instances=1, coalesce=True)
+    scheduler = AsyncIOScheduler(event_loop=asyncio.get_running_loop())
+    scheduler.add_job(update_layermetadatatable, "cron", [], second="*", jitter=0, max_instances=1, coalesce=True)
     scheduler.start()
 
     yield
