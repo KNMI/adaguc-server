2a31a9f38ea01ed3beb82364a95b92b45c0271fd
Ernst de Vreede <ernst.de.vreede@knmi.nl>
2025-03-26 11:11:13 +0100

    Added checks for layers without dims in EDR code

--- python/python_fastapi_server/routers/netcdf_to_covjson.py
+++ python/python_fastapi_server/routers/netcdf_to_covjson.py
@@ -89,9 +89,10 @@ def get_proj_info_from_proj_string(projstring: str) -> GeoReferenceInfo:
 
 
 def get_dim(metadata: dict, dimname: str):
-    for dim in metadata["dims"]:
-        if dimname == metadata["dims"][dim]["cdfName"]:
-            return metadata["dims"][dim]
+    if "dims" in metadata and metadata["dims"] is not None:
+        for dim in metadata["dims"]:
+            if dimname == metadata["dims"][dim]["cdfName"]:
+                return metadata["dims"][dim]
     return {}
 
 
