017ef1a1236ecf5020abc06010f28ea23ee9682b
Belén Torrente <belen.torrentetorrente@knmi.nl>
2025-06-27 15:40:01 +0200

    Add arial test for contour lines

--- tests/AdagucTests/TestWMS.py
+++ tests/AdagucTests/TestWMS.py
@@ -1988,6 +1988,37 @@ class TestWMS(unittest.TestCase):
             )
         )  # Allowed pixel difference is huge, but only for very small number of pixels
 
+
+    def test_WMSGetMap_dashed_contour_lines_arial(self):
+        AdagucTestTools().cleanTempDir()
+        config = (
+            ADAGUC_PATH
+            + "/data/config/adaguc.tests.dataset.xml,"
+            + ADAGUC_PATH
+            + "/data/config/datasets/adaguc.tests.dashedcontourlines-arial.xml"
+        )
+        env = {"ADAGUC_CONFIG": config}
+        # pylint: disable=unused-variable
+        status, data, headers = AdagucTestTools().runADAGUCServer(
+            args=["--updatedb", "--config", config], env=self.env, isCGI=False
+        )
+        self.assertEqual(status, 0)
+        filename = "test_WMSGetMap_dashed_contour_lines_arial.png"
+        status, data, headers = AdagucTestTools().runADAGUCServer(
+            "dataset=adaguc.tests.dashedcontourlines&SERVICE=WMS&&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=baselayer,dashed_contour_lines,overlay&WIDTH=773&HEIGHT=927&CRS=EPSG%3A3857&BBOX=-1572926.437674431,4261090.143221738,2101038.6845761645,8666996.570552012&STYLES=testdata_style_manycontours%2Fcontour&FORMAT=image/png32&TRANSPARENT=TRUE&",
+            env=env,
+        )
+        AdagucTestTools().writetofile(self.testresultspath + filename, data.getvalue())
+        self.assertEqual(status, 0)
+        self.assertTrue(
+            AdagucTestTools().compareImage(
+                self.expectedoutputsspath + filename,
+                self.testresultspath + filename,
+                4,
+                0.02,
+            )
+        )  # Allowed pixel difference is huge, but only for very small number of pixels
+
     def test_WMSGetMap_allow_encoded_proj4_params_attribute(self):
         ### This tests the rotated_pole projection ###
         AdagucTestTools().cleanTempDir()
