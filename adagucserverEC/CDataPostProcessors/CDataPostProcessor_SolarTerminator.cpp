7f8dafe777d470e5421174a18f997dd7667a6354
Belén Torrente <belen.torrentetorrente@knmi.nl>
2025-11-04 13:35:18 +0100

    Remove X, Y vars/dims from postprocessor

--- adagucserverEC/CDataPostProcessors/CDataPostProcessor_SolarTerminator.cpp
+++ adagucserverEC/CDataPostProcessors/CDataPostProcessor_SolarTerminator.cpp
@@ -49,12 +49,8 @@ int CDPPSolarTerminator::execute(CServerConfig::XMLE_DataPostProc *proc, CDataSo
     CDataSource::DataObject *newDataObject = dataSource->getDataObject(0);
     newDataObject->variableName.copy(newVariableName.c_str());
 
-    // Copy bounding box of screen
+    // Copy bounding box of screen (can this code be moved?)
     auto *geo = dataSource->srvParams->Geo;
-    double dfBBOX[] = {geo->dfBBOX[0], geo->dfBBOX[1], geo->dfBBOX[2], geo->dfBBOX[3]};
-    size_t width = geo->dWidth;
-    size_t height = geo->dHeight;
-
     dataSource->nativeProj4 = geo->CRS;
     dataSource->dWidth = geo->dWidth;
     dataSource->dHeight = geo->dHeight;
@@ -63,101 +59,9 @@ int CDPPSolarTerminator::execute(CServerConfig::XMLE_DataPostProc *proc, CDataSo
     dataSource->dfBBOX[2] = geo->dfBBOX[2];
     dataSource->dfBBOX[3] = geo->dfBBOX[3];
 
-    // Create new dimensions and variables (X,Y,T)
-
-    CDF::Dimension *dimX = new CDF::Dimension();
-    dimX->name = "xet";
-    dimX->setSize(width);
-    newDataObject->cdfObject->addDimension(dimX);
-
-    // Define the X variable using the X dimension
-    CDF::Variable *varX = new CDF::Variable();
-    varX->setType(CDF_DOUBLE);
-    varX->name.copy("xet");
-    varX->isDimension = true;
-    varX->dimensionlinks.push_back(dimX);
-    newDataObject->cdfObject->addVariable(varX);
-    CDF::allocateData(CDF_DOUBLE, &varX->data, dimX->length);
-
-    // Set the bbox in the data, since the virtual grid is 2x2 pixels we can directly apply the bbox
-    ((double *)varX->data)[0] = dfBBOX[0];
-    ((double *)varX->data)[1] = dfBBOX[2];
-
-    // For y dimension
-    CDF::Dimension *dimY = new CDF::Dimension();
-    dimY->name = "yet";
-    dimY->setSize(height);
-    newDataObject->cdfObject->addDimension(dimY);
-
-    // Define the Y variable using the X dimension
-    CDF::Variable *varY = new CDF::Variable();
-    varY->setType(CDF_DOUBLE);
-    varY->name.copy("yet");
-    varY->isDimension = true;
-    varY->dimensionlinks.push_back(dimY);
-    newDataObject->cdfObject->addVariable(varY);
-    CDF::allocateData(CDF_DOUBLE, &varY->data, dimY->length);
-
-    ((double *)varY->data)[0] = dfBBOX[1];
-    ((double *)varY->data)[1] = dfBBOX[3];
-
-    dataSource->formatConverterActive = true;
-    // Define the Solar Terminator variable using the defined dimensions, and set the right attributes
-    CDF::Variable *solTVar = new CDF::Variable();
-    solTVar->setType(CDF_FLOAT);
-    float fillValue[] = {-1};
-    solTVar->setAttribute("_FillValue", solTVar->getType(), fillValue, 1);
-    solTVar->dimensionlinks.push_back(dimY);
-    solTVar->dimensionlinks.push_back(dimX);
-    solTVar->setType(CDF_FLOAT);
-    solTVar->name = "SolT";
-    CDBDebug("Setting units");
-    solTVar->setAttributeText("units", "categories");
-    solTVar->setAttributeText("grid_mapping", "projection");
-    newDataObject->cdfObject->addVariable(solTVar);
-    newDataObject->cdfVariable = solTVar;
-
-    newDataObject->cdfVariable->setCustomReader(CDF::Variable::CustomMemoryReaderInstance);
-
-    newDataObject->cdfVariable->setSize(dataSource->dWidth * dataSource->dHeight);
-
-    // Make the width and height of the new 2D adaguc field the same as the viewing window
-    dataSource->dWidth = dataSource->srvParams->Geo->dWidth;
-    dataSource->dHeight = dataSource->srvParams->Geo->dHeight;
-
     // Width and height of the dataSource need to be at least 2 in this case.
     if (dataSource->dWidth < 2) dataSource->dWidth = 2;
     if (dataSource->dHeight < 2) dataSource->dHeight = 2;
-
-    // Get the X and Y dimensions previousely defined and adjust them to the new settings and new grid (Grid in screenview space)
-    dimX->setSize(dataSource->dWidth);
-    dimY->setSize(dataSource->dHeight);
-
-    // Re-allocate data for these coordinate variables with the new grid size
-    CDF::allocateData(CDF_DOUBLE, &varX->data, dimX->length);
-    CDF::allocateData(CDF_DOUBLE, &varY->data, dimY->length);
-
-    // Calculate the gridsize, allocate data and fill the data with a fillvalue
-    size_t fieldSize = dimX->length * dimY->length;
-    newDataObject->cdfVariable->setSize(fieldSize);
-    CDF::allocateData(newDataObject->cdfVariable->getType(), &(newDataObject->cdfVariable->data), fieldSize);
-    CDF::fill(newDataObject->cdfVariable->data, newDataObject->cdfVariable->getType(), fillValue[0], fieldSize);
-
-    // Calculate cellsize and offset of the echo toppen (ET) 2D virtual grid, using the same grid as the screenspace
-    double cellSizeX = (dataSource->srvParams->Geo->dfBBOX[2] - dataSource->srvParams->Geo->dfBBOX[0]) / double(dataSource->dWidth);
-    double cellSizeY = (dataSource->srvParams->Geo->dfBBOX[3] - dataSource->srvParams->Geo->dfBBOX[1]) / double(dataSource->dHeight);
-    double offsetX = dataSource->srvParams->Geo->dfBBOX[0];
-    double offsetY = dataSource->srvParams->Geo->dfBBOX[1];
-
-    // Fill in the X and Y dimensions with the array of coordinates
-    for (size_t j = 0; j < dimX->length; j++) {
-      double x = offsetX + double(j) * cellSizeX + cellSizeX / 2;
-      ((double *)varX->data)[j] = x;
-    }
-    for (size_t j = 0; j < dimY->length; j++) {
-      double y = offsetY + double(j) * cellSizeY + cellSizeY / 2;
-      ((double *)varY->data)[j] = y;
-    }
   }
   if (mode == CDATAPOSTPROCESSOR_RUNAFTERREADING) {
     CDBDebug("CDATAPOSTPROCESSOR_RUNAFTERREADING::Applying SOLARTERMINATOR");
