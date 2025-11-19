b7a9712db0ffd03d237c496244a623954adcb3c4
Maarten Plieger <maarten.plieger@knmi.nl>
2025-11-19 12:54:12 +0100

    Allow more color difference for generic tests

--- tests/AdagucTests/TestGenericImageWarper.py
+++ tests/AdagucTests/TestGenericImageWarper.py
@@ -118,7 +118,7 @@ class TestGenericImageWarper(unittest.TestCase):
                 AdagucTestTools().compareImage(
                     self.expectedoutputsspath + filename,
                     self.testresultspath + filename,
-                    80,
+                    80,0.03
                 )
             )
 
