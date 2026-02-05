170cc07b8d9a031f709916e22786d927db952af5
Belén Torrente <belen.torrentetorrente@knmi.nl>
2026-02-05 14:49:38 +0100

    Simplify tests with update_db

--- tests/AdagucTests/TestWMS.py
+++ tests/AdagucTests/TestWMS.py
@@ -11,6 +11,7 @@ import datetime
 from adaguc.AdagucTestTools import AdagucTestTools
 from lxml import etree, objectify
 import pytest
+from conftest import make_adaguc_env, update_db
 
 ADAGUC_PATH = os.environ["ADAGUC_PATH"]
 
@@ -2671,20 +2672,8 @@ class TestWMS(unittest.TestCase):
 
     def test_WMSGetMap_SolarTerminatorEquinox(self):
         # Testing the solar terminator on March 21, 2023
-        AdagucTestTools().cleanTempDir()
-        config = (
-            ADAGUC_PATH
-            + "/data/config/adaguc.tests.dataset.xml,"
-            + ADAGUC_PATH
-            + "/data/config/datasets/adaguc_tests_solarterminator.xml"
-        )
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False
-        )
-        self.assertEqual(status, 0)
-
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc_tests_solarterminator.xml")
+        update_db(env)
         filename = "test_WMSGetMap_SolarTerminatorEquinox.png"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "DATASET=solarterminator&SERVICE=WMS&&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=solarterminator&WIDTH=200&HEIGHT=200&CRS=EPSG%3A3857&BBOX=-27591378.677139122,-15819675.465716192,24482445.32432534,23920874.430138264&STYLES=solt_twilight&FORMAT=image/png&TRANSPARENT=TRUE&&time=2023-03-21T00%3A00%3A00Z&",
@@ -2699,20 +2688,8 @@ class TestWMS(unittest.TestCase):
 
     def test_WMSGetMap_SolarTerminatorSolstice(self):
         # Testing the solar terminator on December 21, 2022
-        AdagucTestTools().cleanTempDir()
-        config = (
-            ADAGUC_PATH
-            + "/data/config/adaguc.tests.dataset.xml,"
-            + ADAGUC_PATH
-            + "/data/config/datasets/adaguc_tests_solarterminator.xml"
-        )
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False
-        )
-        self.assertEqual(status, 0)
-
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc_tests_solarterminator.xml")
+        update_db(env)
         filename = "test_WMSGetMap_SolarTerminatorSolstice.png"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "DATASET=solarterminator&SERVICE=WMS&&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=solarterminator&WIDTH=200&HEIGHT=200&CRS=EPSG%3A3857&BBOX=-27591378.677139122,-15819675.465716192,24482445.32432534,23920874.430138264&STYLES=solt_twilight&FORMAT=image/png&TRANSPARENT=TRUE&&time=2022-12-21T00%3A00%3A00Z&",
@@ -2728,20 +2705,8 @@ class TestWMS(unittest.TestCase):
 
     def test_WMSGetMap_SolarTerminatorSolstice_continuous(self):
         # Testing the solar terminator on December 21, 2022 with continuous colours
-        AdagucTestTools().cleanTempDir()
-        config = (
-            ADAGUC_PATH
-            + "/data/config/adaguc.tests.dataset.xml,"
-            + ADAGUC_PATH
-            + "/data/config/datasets/adaguc_tests_solarterminator.xml"
-        )
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False
-        )
-        self.assertEqual(status, 0)
-
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc_tests_solarterminator.xml")
+        update_db(env)
         filename = "test_WMSGetMap_SolarTerminatorSolstice_continuous.png"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "DATASET=solarterminator&SERVICE=WMS&&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=solarterminator&WIDTH=200&HEIGHT=200&CRS=EPSG%3A3857&BBOX=-27591378.677139122,-15819675.465716192,24482445.32432534,23920874.430138264&STYLES=solt_continuous&FORMAT=image/png&TRANSPARENT=TRUE&&time=2022-12-21T00%3A00%3A00Z&",
@@ -2757,20 +2722,8 @@ class TestWMS(unittest.TestCase):
 
     def test_WMSGetMap_SolarTerminatorQuarterPoint(self):
         # Testing the solar terminator on August 7, 2000
-        AdagucTestTools().cleanTempDir()
-        config = (
-            ADAGUC_PATH
-            + "/data/config/adaguc.tests.dataset.xml,"
-            + ADAGUC_PATH
-            + "/data/config/datasets/adaguc_tests_solarterminator.xml"
-        )
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False
-        )
-        self.assertEqual(status, 0)
-
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc_tests_solarterminator.xml")
+        update_db(env)
         filename = "test_WMSGetMap_SolarTerminatorQuarterPoint.png"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "DATASET=solarterminator&SERVICE=WMS&&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=solarterminator&WIDTH=200&HEIGHT=200&CRS=EPSG%3A3857&BBOX=-27591378.677139122,-15819675.465716192,24482445.32432534,23920874.430138264&STYLES=solt_twilight&FORMAT=image/png&TRANSPARENT=TRUE&&time=2000-08-07T00%3A00%3A00Z&",
@@ -2785,20 +2738,8 @@ class TestWMS(unittest.TestCase):
 
     def test_WMSGetLegendGraphic_SolarTerminator_twilight(self):
         # Testing the solar terminator legend
-        AdagucTestTools().cleanTempDir()
-        config = (
-            ADAGUC_PATH
-            + "/data/config/adaguc.tests.dataset.xml,"
-            + ADAGUC_PATH
-            + "/data/config/datasets/adaguc_tests_solarterminator.xml"
-        )
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False
-        )
-        self.assertEqual(status, 0)
-
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc_tests_solarterminator.xml")
+        update_db(env)
         filename = "test_WMSGetLegendGraphic_SolarTerminator_twilight.png"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "DATASET=solarterminator&SERVICE=WMS&&version=1.3.0&service=WMS&request=GetLegendGraphic&layer=solarterminator&format=image/png&STYLE=solt_twilight&layers=solarterminator&&&transparent=true&",
@@ -2814,20 +2755,8 @@ class TestWMS(unittest.TestCase):
 
     def test_WMSGetLegendGraphic_SolarTerminator_continuous(self):
         # Testing the solar terminator legend
-        AdagucTestTools().cleanTempDir()
-        config = (
-            ADAGUC_PATH
-            + "/data/config/adaguc.tests.dataset.xml,"
-            + ADAGUC_PATH
-            + "/data/config/datasets/adaguc_tests_solarterminator.xml"
-        )
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False
-        )
-        self.assertEqual(status, 0)
-
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc_tests_solarterminator.xml")
+        update_db(env)
         filename = "test_WMSGetLegendGraphic_SolarTerminator_continuous.png"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "DATASET=solarterminator&SERVICE=WMS&&version=1.3.0&service=WMS&request=GetLegendGraphic&layer=solarterminator&format=image/png&STYLE=solt_continuous&layers=solarterminator&&&transparent=true&",
@@ -2843,19 +2772,8 @@ class TestWMS(unittest.TestCase):
 
     def test_WMSGetFeatureInfo_SolarTerminator(self):
         # Testing the solar terminator feature info
-        AdagucTestTools().cleanTempDir()
-        config = (
-            ADAGUC_PATH
-            + "/data/config/adaguc.tests.dataset.xml,"
-            + ADAGUC_PATH
-            + "/data/config/datasets/adaguc_tests_solarterminator.xml"
-        )
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False
-        )
-
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc_tests_solarterminator.xml")
+        update_db(env)
         filename = "test_WMSGetFeatureInfo_SolarTerminator.html"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "dataset=solt&&SERVICE=WMS&REQUEST=GetFeatureInfo&VERSION=1.3.0&LAYERS=solarterminator&QUERY_LAYERS=solarterminator&CRS=EPSG%3A3857&BBOX=-48040284.36018957,-50974535.88541803,-10040284.360189572,62500602.23612894&WIDTH=362&HEIGHT=1081&I=354&J=481&INFO_FORMAT=text/html&STYLES=&&time=2026-01-05T16%3A40%3A00Z&",
@@ -2870,15 +2788,8 @@ class TestWMS(unittest.TestCase):
 
     def test_WMSGetFeatureInfo_timeseries_SolarTerminator(self):
         # Testing the solar terminator feature info
-        AdagucTestTools().cleanTempDir()
-        config = (ADAGUC_PATH + "/data/config/adaguc.tests.dataset.xml," +
-                  ADAGUC_PATH +
-                  "/data/config/datasets/adaguc_tests_solarterminator.xml")
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False)
-
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc_tests_solarterminator.xml")
+        update_db(env)
         filename = "test_WMSGetFeatureInfo_timeseries_SolarTerminator.html"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "dataset=solt&service=WMS&request=GetFeatureInfo&version=1.3.0&layers=solarterminator&query_layers=solarterminator&crs=EPSG%3A3857&bbox=-19000000%2C-78286307.05394192%2C19000000%2C78286307.05394192&width=241&height=993&i=180&j=366&format=image%2Fgif&info_format=application%2Fjson&time=2025-10-01T00%3A00%3A00Z%2F2025-10-02T00%3A00%3A00Z&",
@@ -2894,16 +2805,9 @@ class TestWMS(unittest.TestCase):
         )
 
     def test_WMSGetCapabilities_SolarTerminator(self):
-        # Testing the solar terminator feature info
-        AdagucTestTools().cleanTempDir()
-        config = (ADAGUC_PATH + "/data/config/adaguc.tests.dataset.xml," +
-                  ADAGUC_PATH +
-                  "/data/config/datasets/adaguc.tests.solarterminator-custom.xml")
-        env = {"ADAGUC_CONFIG": config}
-        # pylint: disable=unused-variable
-        status, data, headers = AdagucTestTools().runADAGUCServer(
-            args=["--updatedb", "--config", config], env=self.env, isCGI=False)
-
+        # Testing the solar terminator capabilities
+        env = make_adaguc_env("{ADAGUC_PATH}/data/config/datasets/adaguc.tests.solarterminator-custom.xml")
+        update_db(env)
         filename = "test_WMSGetCapabilities_SolarTerminator.xml"
         status, data, headers = AdagucTestTools().runADAGUCServer(
             "dataset=solt&SERVICE=WMS&request=GetCapabilities",
