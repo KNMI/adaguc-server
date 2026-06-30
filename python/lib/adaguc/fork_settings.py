9701b50af852b0193725689f752345f92a4546b1
Matthijs Grünbauer <matthijs.grunbauer@knmi.nl>
2026-06-30 12:45:12 +0200

    Fix various small issues

--- python/lib/adaguc/fork_settings.py
+++ python/lib/adaguc/fork_settings.py
@@ -1,7 +1,12 @@
 import os
+from pathlib import Path
+
+ADAGUC_FORK_SOCKET_NAME = "adaguc.socket"
 
 
-ADAGUC_FORK_SOCKET_PATH = "adaguc.socket"
+def get_fork_socket_path() -> str:
+    adaguc_path = os.getenv("ADAGUC_PATH", "./")
+    return str(Path(adaguc_path) / ADAGUC_FORK_SOCKET_NAME)
 
 
 def is_fork_enabled() -> bool:
