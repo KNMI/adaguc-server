c1a4972b43953441b5da765eaf741aa404457d81
Maarten Plieger <maarten.plieger@knmi.nl>
2026-07-01 19:44:33 +0200

    Fixed typo previousely -> previously

--- hclasses/testhclasses.cpp
+++ hclasses/testhclasses.cpp
@@ -301,11 +301,9 @@ TEST(string, startsWith) {
   LONGS_EQUAL(true, CT::startsWith(valueToCheck, "Hello"));
   LONGS_EQUAL(true, CT::startsWith(valueToCheck, ""));
 
-  // Note, specifying a stdstring with a starting and ending double qoute as part of the string did not work previousely.
+  // Note, specifying a stdstring with a starting and ending double qoute as part of the string did not work previously.
   std::string n = "\" +proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs +<>\"";
   LONGS_EQUAL(true, CT::startsWith(n, "\""));
-
-  // printf("====================================================== YO! %zu", n.rfind("\""));
 }
 
 TEST(string, encodeXml) {
